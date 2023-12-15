df11765e499b6d80a8de61f7248d027d278e14fa
vitória ketlyn <142404330+Vitoriaketlyn@users.noreply.github.com>
2023-12-15 14:28:12 -0300

    questões

---
+++ desafios funpro2/Questoes/d001/script.js
@@ -0,0 +1,15 @@
+bnt1 = document.getElementById("btn1");
+
+bnt1.addEventListener("click", function () {
+  alert("Voce clicou no 1º botão!");
+});
+
+document.getElementById("btn2").addEventListener("click", function () {
+  alert("Voce clicou no 2º botão!");
+});
+
+document.getElementById("btn3").addEventListener("click", function () {
+  alert("Voce clicou no 3º botão!");
+});
+
+bnt1.addEventListener("");
